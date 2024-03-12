1c0d3c1d576a255cbc89b04e19b4c3728cc7b07d
viktor.weilenmann <viktor.weilenmann@students.fhnw.ch>
2024-03-12 18:40:56 +0100

    implements basic sBox

--- src/main/java/org/example/Decipher.java
+++ src/main/java/org/example/Decipher.java
@@ -1,24 +1,31 @@
 package org.example;
 
 import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Decipher {
 
     private Integer[] permutation;
-    private var sBox;
-    private var sInv;
+    private Map sBox;
+    private Map sInv;
     private KeyMgmt key;
 
-    public Decipher(Integer[] permutation, var sBox, KeyMgmt key) {
+    public Decipher(Integer[] permutation, Map<Integer,Integer> sBox, KeyMgmt key) {
         this.permutation = permutation;
         this.sBox = sBox;
         this.key=key;
         this.sInv=inverse(sBox);
     }
 
-    private var inverse(var sBox) {
-        // TODO: inverse sBox
-        return null;
+    private Map inverse(Map sBox) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (Object o : sBox.keySet()) {
+            Integer key = (Integer) o;
+            Integer value = (Integer) sBox.get(key);
+            map.put(value,key);
+        }
+        return map;
     }
 
     public int doDecipher(int text){
