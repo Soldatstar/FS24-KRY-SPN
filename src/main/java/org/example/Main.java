0e700884b98528ce877fed342cb783503c437c53
viktor.weilenmann <viktor.weilenmann@students.fhnw.ch>
2024-03-18 09:40:27 +0100

    combine cipher into one class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,40 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Integer[] permutation = {0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15};
-        int x = 1;
-        int y = 0;
+    public static void main(String[] args){
+        int r = 4;
+        int s = 32;
+        int n = 4;
+        int m = 4;
+        String key = "00010001001010001000110000000000";
+        int[] beta = {0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15};
+        KeyMgmt keys = new KeyMgmt(r,s,n,m,key,beta);
+        String klartext = "0001001010001111";
+        Map<String,String> sBox = new HashMap<>();
+        sBox.put("0000","1110");// 0 E
+        sBox.put("0001","0100");
+        sBox.put("0010","1101");// 2 D
+        sBox.put("0011","0001");
+        sBox.put("0100","0010");//4 2
+        sBox.put("0101","1111");
+        sBox.put("0110","1011");//6 B
+        sBox.put("0111","1000");
+        sBox.put("1000","0011");//8 3
+        sBox.put("1001","1010");
+        sBox.put("1010","0110");//10 6
+        sBox.put("1011","1100");
+        sBox.put("1100","0101");//12 5
+        sBox.put("1101","1001");
+        sBox.put("1110","0000");//14 0
+        sBox.put("1111","0111");
 
-        System.out.println(x^y);
+        cipher encipher = new cipher(r,n,m,beta,sBox,keys,klartext);
+        String chiffretext = encipher.chiffretext();
+        System.out.println("klartext: " + klartext);
+        System.out.println("Chiffretext: " + chiffretext);
+        System.out.println("Klartext: " + encipher.dechiffretext(chiffretext));
     }
 }
--- src/main/java/org/example/cipher.java
+++ src/main/java/org/example/cipher.java
@@ -3,11 +3,12 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Encipher {
+public class cipher {
 
     private int r,n,m;
     private int[] beta;
     private Map<String,String> sBox;
+    private Map<String,String> sInv;
     private KeyMgmt keys;
     private int[] klartext;
 
@@ -20,45 +21,55 @@ public class Encipher {
      * keys: Generates the roundkeys
      * klartext: Text to encript, as bitstring
      * */
-    public Encipher(int r, int n, int m, int[] beta, Map<String,String> sBox, KeyMgmt keys, String klartext) {
+    public cipher(int r, int n, int m, int[] beta, Map<String,String> sBox, KeyMgmt keys, String klartext) {
         this.r = r;
         this.n = n;
         this.m = m;
         this.beta = beta;
         this.sBox = sBox;
+        this.sInv = inverse(sBox);
         this.keys = keys;
         klartextIntoArray(klartext);
     }
 
+    private Map<String,String> inverse(Map<String, String> sBox) {
+        Map<String,String> map = new HashMap<>();
+        for (String o : sBox.keySet()) {
+            String value = sBox.get(o);
+            map.put(value, o);
+        }
+        return map;
+    }
+
     //TODO Delete this main function befor submitting the code to vogt
     public static void main(String[] args){
-        int r = 3;
-        int s = 24;
+        int r = 4;
+        int s = 32;
         int n = 4;
-        int m = 3;
-        String key = "000110101111110000000111";
-        int[] beta = {4,5,8,9,0,1,10,11,2,3,6,7};
+        int m = 4;
+        String key = "00010001001010001000110000000000";
+        int[] beta = {0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15};
         KeyMgmt keys = new KeyMgmt(r,s,n,m,key,beta);
-        String klartext = "111101010110";
+        String klartext = "0001001010001111";
         Map<String,String> sBox = new HashMap<>();
-        sBox.put("0000","0101");
+        sBox.put("0000","1110");// 0 E
         sBox.put("0001","0100");
-        sBox.put("0010","1101");
+        sBox.put("0010","1101");// 2 D
         sBox.put("0011","0001");
-        sBox.put("0100","0011");
-        sBox.put("0101","1100");
-        sBox.put("0110","1011");
+        sBox.put("0100","0010");//4 2
+        sBox.put("0101","1111");
+        sBox.put("0110","1011");//6 B
         sBox.put("0111","1000");
-        sBox.put("1000","1010");
-        sBox.put("1001","0010");
-        sBox.put("1010","0110");
-        sBox.put("1011","1111");
-        sBox.put("1100","1001");
-        sBox.put("1101","1110");
-        sBox.put("1110","0000");
+        sBox.put("1000","0011");//8 3
+        sBox.put("1001","1010");
+        sBox.put("1010","0110");//10 6
+        sBox.put("1011","1100");
+        sBox.put("1100","0101");//12 5
+        sBox.put("1101","1001");
+        sBox.put("1110","0000");//14 0
         sBox.put("1111","0111");
 
-        Encipher encipher = new Encipher(r,n,m,beta,sBox,keys,klartext);
+        cipher encipher = new cipher(r,n,m,beta,sBox,keys,klartext);
         System.out.println("Chiffretext: " + encipher.chiffretext());
     }
 
@@ -148,6 +159,45 @@ public class Encipher {
         }
     }
 
+    private int[] sboxInverseOperation(int[] text){
+        String toMap = "";
+        String mapping = "";
+
+        for(int x = 0; x < m; x++){
+            for(int y = x*n; y < x*n + n; y++){
+                toMap += text[y];
+            }
+
+            if(!sInv.containsKey(toMap)){
+                throw new IllegalArgumentException("No mapping in sbox for " + toMap);
+            }
+            else{
+                mapping += sInv.get(toMap);
+            }
+
+            toMap = "";
+        }
+
+        if(mapping.length() != n*m){
+            throw new IllegalArgumentException("sBox has values with wrong length");
+        }
+        else{
+            int[] result = new int[n*m];
+            for(int z = 0; z < n*m; z++){
+                if(mapping.charAt(z) == '1'){
+                    result[z] = 1;
+                }
+                else if(mapping.charAt(z) == '0'){
+                    result[z] = 0;
+                }
+                else{
+                    throw new IllegalArgumentException("sBox has values not equal 1 or 0");
+                }
+            }
+            return result;
+        }
+    }
+
     /**
      * Text will be permutated according beta
      * Bit at index i has the index beta[i] after the permutation
@@ -203,6 +253,34 @@ public class Encipher {
             chiffre += result[j];
         }
         return chiffre;
+    }
+
+    public String dechiffretext(String chiffretext){
+        int[] result = new int[n*m];
+        klartextIntoArray(chiffretext);
+
+        for(int i = 0; i < r+1; i++){
+
+            if(i == 0){
+                result = xor(klartext,keys.getKeyForDecription(0));
+            }
+            else if(i == r){
+                int[] tmp = sboxInverseOperation(result);
+                result = xor(tmp,keys.getKeyForDecription(r));
+            }
+            else{
+                int[] tmp = sboxInverseOperation(result);
+                tmp = permutation(tmp);
+                result = xor(tmp,keys.getKeyForDecription(i));
+            }
+
+        }
+
+        String klartext = "";
+        for(int j = 0; j < n*m; j++){
+            klartext += result[j];
+        }
+        return klartext;
     }
 
 }
